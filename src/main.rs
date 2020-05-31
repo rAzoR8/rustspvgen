bd12eeac76aa07d6353518315eb4bc83c2d968f7
Fabian Wahlster <fwahlster@outlook.com>
2020-05-31 13:43:20 +0200

    read grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::File;
+//use std::io::Read;
+use std::env;
+
+extern crate serde_json;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file = File::open(&args[1]).expect("file should open read only");
+    let json: serde_json::Value = serde_json::from_reader(file).expect("file should be proper JSON");
+    println!("Please call {} at the number {}", json["magic_number"], json["instructions"][0]["opname"]);
 }
