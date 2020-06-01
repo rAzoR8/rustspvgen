695c4e2627ceee060266ff5544a2c33fd15cedfc
Fabian Wahlster <fwahlster@outlook.com>
2020-06-01 16:09:25 +0200

    deserialize instruction, fix operandkinds, print some stuff for testing

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,45 @@ struct Grammar {
     major_version: i32,
     minor_version: i32,
     revision: i32,
+    instructions: Vec<Instruction>,
     operand_kinds: Vec<OperandKinds>
+}
+
+#[derive(Deserialize)]
+struct Instruction
+{
+    opname: String,
+    class: String,
+    opcode: u32,
+    operands: Option<Vec<Operand>>
+}
+
+#[derive(Deserialize)]
+struct Operand
+{
+    kind: String,
+    quantifier: Option<String>,
+    name: Option<String>
 }
 
 #[derive(Deserialize)]
 struct OperandKinds {
     category: String,
     kind: String,
-    enumerants: Vec<Enumerants>
+    enumerants: Option<Vec<Enumerants>>
 }
 
+#[derive(Deserialize)]
+enum EnumValue
+{
+    hex(String),
+    int(u32)
+}
 
 #[derive(Deserialize)]
 struct Enumerants {
     enumerant: String,
-    value: String,
+    value: serde_json::Value,
     capabilities: Option<Vec<String>>,
     parameters: Option<Vec<Parameter>>,
     extensions: Option<Vec<String>>,
@@ -44,31 +68,36 @@ struct Parameter
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file = File::open(&args[1]).expect("file should open read only");
-    let spv: Grammar = serde_json::from_reader(file).unwrap();//.expect("file should be proper JSON");
+    let spv: Grammar = serde_json::from_reader(file).expect("file should be proper JSON");
 
     println!("magic {} major {}", spv.magic_number, spv.major_version);
 
+    // for elem in spv.instructions {
+    //     println!("name {} opcode {}", elem.opname, elem.opcode);
+    // }
+
     for elem in spv.operand_kinds {
-        println!("cat {} kind {}", elem.category, elem.kind);
+        match elem.enumerants.as_ref()
+        {
+            Some(v) =>
+            {
+                println!("enum class {} : unsigned int \n{{", elem.kind);
+                for enumval in v {
+                    print!("\t{} = ",enumval.enumerant);
+                    match &enumval.value
+                    {
+                        serde_json::Value::Number(x) => {print!("{},\n",x)},
+                        serde_json::Value::String(s) => {print!("{},\n",s)}
+                        _ => {}
+                    }
+                }
+                println!("}};");
+
+            },
+            None => {}
+        }
     }
-
-    //println!("enum class OperandType : unsigned int \n {{\t");
-
-    // let operands = match json["operand_kinds"] {
-    //     serde_json::Array(ref v) => {
-    //             formatter.write_str("Array(")?;
-    //             Debug::fmt(v, formatter)?;
-    //             formatter.write_str(")")
-    //         }
-    //         _ => {
-    //             formatter.write_str("Object(")?;
-    //             Debug::fmt(v, formatter)?;
-    //             formatter.write_str(")")
-    //         }
-    // };
-
 
     //let mut ofile = File::create("spvgrammer.inl").expect("unable to create file");
     //ofile.write_all(output.as_bytes()).expect("unable to write");
-
 }
